37e9d5c8fcb3b22ad263742b9dd31a4f45738b0d
akio <akio@akios-Mac.local>
2017-12-16 00:36:27 +0100

    init_map

--- include/lemipc.h
+++ include/lemipc.h
@@ -1,14 +1,16 @@
 #ifndef LEMIPC_H
-# define LEMPIC_H
+# define LEMIPC_H
 
 #include "libft.h"
 #include <stdint.h>
+#include <sys/mman.h>
 
-# define MAP_SIZE 8
+# define MAP_LEN 8
+# define MAP_SIZE MAP_LEN * MAP_LEN
 
-typedef struct	s_player {
+typedef struct	s_cell {
 	size_t		team_id;
 	uint32_t	pid;
-}				t_player;
+}				t_cell;
 
 #endif
--- src/lemipc.c
+++ src/lemipc.c
@@ -1,6 +1,37 @@
 #include "lemipc.h"
 
+void	init_map(t_cell cells[MAP_LEN][MAP_LEN])
+{
+	size_t		i;
+
+	i = 0;
+	ft_memset(cells, 0, MAP_SIZE);
+	while (i < MAP_SIZE)
+	{
+		ft_memset(cells[i], 0, MAP_SIZE);
+		i++;
+	}
+}
+
+void	init_shm(t_cell cells[MAP_LEN][MAP_LEN])
+{
+	int		fd;
+	size_t	len;
+	void	*mem;
+
+	len = sizeof(t_cell) * MAP_SIZE;
+	fd = shm_open("/shm-lemipc_map", O_RDWR, 0666);
+	ftruncate(fd, len);
+	mem = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+	ft_memcpy(mem, cells, MAP_SIZE); // not ok(contiguous addr)
+}
+
 int		main(void)
 {
+	t_cell cells[MAP_LEN][MAP_LEN];
+
+	init_map(cells);
+	init_shm(cells);
 	return (0);
 }
