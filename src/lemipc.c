31b375273eb4aa0b7c9d928637de3d4c24202659
Renan PORCON <rporcon@student.42.fr>
2018-01-02 16:31:58 +0100

    add comments

--- src/lemipc.c
+++ src/lemipc.c
@@ -8,18 +8,23 @@ void	communicate()
 
 	ft_memset(&msgbuf, 0, sizeof msgbuf);
 	msgq_getid(&msgq_id);
+	// msgtype = pid to send msg
 	while (1)
 	{
 		if (msgrcv(msgq_id, &msgbuf, sizeof msgbuf.mtext, 0, 0) == -1)
 			perr_exit("communicate msgrcv");
 		printf("message received on queue");
 		// supposition
+		// create another shared memory segment for data
+		// ! players has to played depending to their arrival in game and keep
+		// that order
 		// send msg to each same team id (they have to focus same ennemy)
 		// when msg receive -> tick player status to "has played"
-		// if not good player receive msg -> resend
+		// ??? if not good player receive msg -> resend  ??? 
 		// resend msg to an other member of same team if available
 		// struct to know current status of each player (has played ?)
-		// when all players has played wait the enter if map_print to play next turn
+		// -> check if all players team has played then tick team has played = ok
+		// when all team has played -> wait enter of map_print to play next turn
 	}
 
 }
@@ -35,7 +40,7 @@ int		main(int ac, char **av)
 	ft_memset(cells, 0, MAP_SIZE);
 	get_args(ac, av);
 	sighandle(); // clear only if last process quit
-				 // handle too much processus case
+				 // handle too much processus error case
 	map_get(&map_mem);
 	map_fill(map_mem, cells);
 	map_addplayer(map_mem, cells);
